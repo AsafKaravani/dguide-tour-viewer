fb4cad55bffa55c8cd08c273453eb71f78470699
Asaf <asaf.karavani1@gmail.com>
2022-12-01 18:21:30 +0200

    added prettier

--- apps/tour-viewer/plopfile.js
+++ apps/tour-viewer/plopfile.js
@@ -1,37 +1,37 @@
 export default function (plop) {
   // controller generator
-  plop.setGenerator("View Component", {
-    description: "New View component",
+  plop.setGenerator('View Component', {
+    description: 'New View component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Enter component name:",
+        type: 'input',
+        name: 'name',
+        message: 'Enter component name:',
       },
     ],
     actions: [
       {
-        type: "add",
-        path: "src/{{pascalCase name}}.tsx",
-        templateFile: "_templates/ViewComponent.tsx.hbs",
+        type: 'add',
+        path: 'src/{{pascalCase name}}.tsx',
+        templateFile: '_templates/ViewComponent.tsx.hbs',
       },
     ],
   });
 
-  plop.setGenerator("Smart Component", {
-    description: "New Smart component",
+  plop.setGenerator('Smart Component', {
+    description: 'New Smart component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Enter component name:",
+        type: 'input',
+        name: 'name',
+        message: 'Enter component name:',
       },
     ],
     actions: [
       {
-        type: "add",
-        path: "src/{{pascalCase name}}.tsx",
-        templateFile: "_templates/SmartComponent.tsx.hbs",
+        type: 'add',
+        path: 'src/{{pascalCase name}}.tsx',
+        templateFile: '_templates/SmartComponent.tsx.hbs',
       },
     ],
   });
--- apps/tour-viewer/src/core/localization/I18nProvider.tsx
+++ apps/tour-viewer/src/core/localization/I18nProvider.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react';
 
 type I18nProviderProps = {
   children?: React.ReactNode;
 };
 
-export const I18nProvider: React.FC<I18nProviderProps> = _p => {
-  return (
-    <div>
-      {_p.children}
-    </div>
-  );
+export const I18nProvider: React.FC<I18nProviderProps> = (_p) => {
+  return <div>{_p.children}</div>;
 };
