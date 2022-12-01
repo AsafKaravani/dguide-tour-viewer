6b251ee71bc2269da1c3684f3489adeab6af750d
Asaf <asaf.karavani1@gmail.com>
2022-12-01 20:04:55 +0200

    .

--- apps/tour-viewer/src/core/GlobalProviders.tsx
+++ apps/tour-viewer/src/core/GlobalProviders.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { I18nProvider } from "./localization/I18nProvider";
-import { StateProvider } from "./state/StateProvider";
-import { AppThemeProvider } from "./theme/ThemeProvider";
+import React from 'react';
+import { I18nProvider } from './localization/I18nProvider';
+import { StateProvider } from './state/StateProvider';
+import { AppThemeProvider } from './theme/ThemeProvider';
 
 type GlobalProvidersProps = {
-  children?: React.ReactNode;
+	children?: React.ReactNode;
 };
 
-export const GlobalProviders: React.FC<GlobalProvidersProps> = _p => {
-  return (
-    <AppThemeProvider>    {/** 🎨 Theme Layer */}
-      <I18nProvider>      {/** 🌎 I18n Layer */}
-        <StateProvider>   {/** 🧠 State Layer */}
-          {_p.children}
-        </StateProvider>
-      </I18nProvider>
-    </AppThemeProvider>
-  );
+export const GlobalProviders: React.FC<GlobalProvidersProps> = (_p) => {
+	return (
+		<AppThemeProvider>    {/** 🎨 Theme Layer */}
+			<I18nProvider>      {/** 🌎 I18n Layer */}
+				<StateProvider>   {/** 🧠 State Layer */}
+					{_p.children}
+				</StateProvider>
+			</I18nProvider>
+		</AppThemeProvider>
+	);
 };
