0c7a193f2354a3d6bacefd2a9b739e98de4e5ec8
Asaf <asaf.karavani1@gmail.com>
2022-12-01 02:49:14 +0200

    .

--- apps/tour-viewer/src/core/GlobalProviders.tsx
+++ apps/tour-viewer/src/core/GlobalProviders.tsx
@@ -1,15 +1,12 @@
-import React from 'react'; // we need this to make JSX compile
-
 type GlobalProvidersProps = {
-  title: string;
   children?: React.ReactNode;
 };
 
-export const GlobalProviders: React.FC<GlobalProvidersProps> = React.forwardRef<HTMLDivElement, GlobalProvidersProps>((_p, ref) => {
+export const GlobalProviders: React.FC<GlobalProvidersProps> = _p => {
   return (
-    <div ref={ref}>
-      <p>{_p.title}</p>
+    <div>
+      <p>Global providers works.</p>
       {_p.children}
     </div>
   );
-});
+};
