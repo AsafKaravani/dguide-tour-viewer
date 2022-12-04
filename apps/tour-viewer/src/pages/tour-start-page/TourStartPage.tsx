d99905876696de826dbb7f1b8fe93d743cd66a9c
Asaf <asaf.karavani1@gmail.com>
2022-12-04 03:05:35 +0200

    deployed

--- apps/tour-viewer/src/pages/tour-start-page/TourStartPage.tsx
+++ apps/tour-viewer/src/pages/tour-start-page/TourStartPage.tsx
@@ -8,84 +8,82 @@ type TourStartPageProps = {
 } & BoxProps;
 
 export const TourStartPage =
-	React.memo(
-		React.forwardRef<BoxProps, TourStartPageProps>(
-			function TourStartPage(_p, ref) {
-				return (
-					<Box ref={ref} {..._p}>
-						<Box className='flex items-center justify-center h-52 bg-white'>
-							<ReactPlayer height='100%' url='/videos/sample-video.mp4' controls />
+	React.forwardRef<BoxProps, TourStartPageProps>(
+		function TourStartPage(_p, ref) {
+			return (
+				<Box ref={ref} {..._p}>
+					<Box className='flex items-center justify-center h-52 bg-white'>
+						<ReactPlayer playing height='100%' width='100%' url='videos/sample-video.mp4' controls />
+					</Box>
+					<Box className='p-4'>
+
+						<Typography variant='h1'>
+							Jerusalem of three religions
+						</Typography>
+						<Seperator />
+
+						<UserBlock title='Your guide' name='Raphael' />
+						<Seperator />
+						<Seperator />
+
+						<Typography variant='body2' className='mb-2' fontWeight={800}>
+							Do not forget
+						</Typography>
+						<Box className='flex gap-4'>
+							<TourChip iconType='solid' iconName='map-location' fontSize='0.8em'>
+								2.19 km
+							</TourChip>
+							<TourChip iconType='solid' iconName='clock'>
+								3-4 Hours
+							</TourChip>
+							<TourChip iconType='solid' iconName='battery-bolt fa-rotate-270'>
+								Full Battery
+							</TourChip>
 						</Box>
-						<Box className='p-4'>
+						<Seperator />
 
-							<Typography variant='h1'>
-								Jerusalem of three religions
-							</Typography>
-							<Seperator />
-
-							<UserBlock title='Your guide' name='Raphael' />
-							<Seperator />
-							<Seperator />
-
-							<Typography variant='body2' className='mb-2' fontWeight={800}>
-								Do not forget
-							</Typography>
-							<Box className='flex gap-4'>
-								<TourChip iconType='solid' iconName='map-location' fontSize='0.8em'>
-									2.19 km
-								</TourChip>
-								<TourChip iconType='solid' iconName='clock'>
-									3-4 Hours
-								</TourChip>
-								<TourChip iconType='solid' iconName='battery-bolt fa-rotate-270'>
-									Full Battery
-								</TourChip>
+						<Box className='flex justify-between'>
+							<TourChip iconType='solid' iconName='p' fontSize='0.95em'>
+								<>Parking: &nbsp;</>
+								<Link href="#">Electricity Garden Parking...</Link>
+							</TourChip>
+							<Box className='flex gap-3 items-center'>
+								<img src='images/waze-icon.png' width={20} />
+								<img src='images/google-maps-icon.png' width={20} />
 							</Box>
-							<Seperator />
+						</Box>
+						<Seperator />
 
-							<Box className='flex justify-between'>
-								<TourChip iconType='solid' iconName='p' fontSize='0.95em'>
-									<>Parking: &nbsp;</>
-									<Link href="#">Electricity Garden Parking...</Link>
-								</TourChip>
-								<Box className='flex gap-3 items-center'>
-									<img src='images/waze-icon.png' width={20} />
-									<img src='images/google-maps-icon.png' width={20} />
-								</Box>
-							</Box>
-							<Seperator />
+						<Typography variant='body2' className='mb-4'>
+							<b>Lorem ipsum dolor sit amet?</b>
+							<br />
+							consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
+						</Typography>
+						<Typography variant='body2' className='mb-4'>
+							<b>Lorem ipsum dolor sit amet?</b>
+							<br />
+							consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
+						</Typography>
+						<Typography variant='body2' className='mb-4'>
+							<b>Lorem ipsum dolor sit amet?</b>
+							<br />
+							consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
+						</Typography>
 
-							<Typography variant='body2' className='mb-4'>
-								<b>Lorem ipsum dolor sit amet?</b>
-								<br />
-								consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
+						<Box className='flex flex-col'>
+							<Button className='mb-5'>
+								Get Started
+							</Button>
+
+							<Typography variant='body2' className='mb-2 text-center' fontWeight={800}>
+								Want to use it offline?
 							</Typography>
-							<Typography variant='body2' className='mb-4'>
-								<b>Lorem ipsum dolor sit amet?</b>
-								<br />
-								consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
-							</Typography>
-							<Typography variant='body2' className='mb-4'>
-								<b>Lorem ipsum dolor sit amet?</b>
-								<br />
-								consectetur adipiscing elit. At dolor tellus dolor facilisis odio non dignissim.
-							</Typography>
-
-							<Box className='flex flex-col'>
-								<Button className='mb-5'>
-									Get Started
-								</Button>
-
-								<Typography variant='body2' className='mb-2 text-center' fontWeight={800}>
-									Want to use it offline?
-								</Typography>
-								<Button color='secondary'>
-									Download
-								</Button>
-							</Box>
+							<Button color='secondary'>
+								Download
+							</Button>
 						</Box>
 					</Box>
-				);
-			}
-		)
+				</Box>
+			);
+		}
 	);
