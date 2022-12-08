566e6a17fd344d7578029011c36b5a5961e3a2de
Assaf Koren <assafnoahkoren@gmail.com>
2022-12-08 17:33:45 +0200

    .

--- apps/tour-viewer/src/core/layouts/RootLayout.tsx
+++ apps/tour-viewer/src/core/layouts/RootLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '@app/core/layouts/Header';
 import { RtlLayer } from '@app/core/theme/RtlLayer';
 
+
 type RootLayoutProps = {
 	children?: React.ReactNode;
 };
