7f0c42e38fc644d964f511fbe677165a07b3184f
Assaf Koren <assafnoahkoren@gmail.com>
2022-12-11 01:05:51 +0200

    integrated routing service

--- apps/tour-viewer/src/features/map/tour-layer/TourLayer.tsx
+++ apps/tour-viewer/src/features/map/tour-layer/TourLayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Marker, MarkerLayer } from 'react-leaflet-marker';
 import { TourPin } from '@app/shared/components';
 import { useRecoilValue } from 'recoil';
@@ -7,18 +7,31 @@ import { useLeafletContext } from '@react-leaflet/core';
 import { Pane, Polyline } from 'react-leaflet';
 import { useGlobalHooks } from '@app/core/hooks/global-hooks';
 import { LatLngExpression } from 'leaflet';
+import { service_routeService } from '@app/shared/services/route.service';
+//5b3ce3597851110001cf6248ad8483d00dbb489e958c465e60ca3baa
 
 export const TourLayer = React.memo(function TourLayer() {
 	const state_TourState = useRecoilValue(atom_tourState);
-	const tourWaypoints: LatLngExpression[] =
+	const tourWaypoints: [number, number][] =
 		state_TourState?.stops.map((stop) => [
 			stop.stop_location.latitude,
 			stop.stop_location.longitude,
 		]) ?? [];
+
+	const tourWaypoints_latLngReversed: [number, number][] =
+		state_TourState?.stops.map((stop) => [
+			stop.stop_location.longitude,
+			stop.stop_location.latitude,
+		]) ?? [];
+
+	const [walkingWaypoints, setWalkingWaypoints] = useState([]);
+
 	const leaflet = useLeafletContext();
 	const _g = useGlobalHooks();
 
 	useEffect(() => {
+		console.log(state_TourState);
+
 		const firstStop = state_TourState?.stops[0];
 		if (firstStop?.stop_location)
 			leaflet.map.panTo(
@@ -28,42 +41,52 @@ export const TourLayer = React.memo(function TourLayer() {
 					animate: true,
 				}
 			);
-		setTimeout(() => {
-			console.log(leaflet.map.getPanes());
-		}, 1000);
+
+		(async () => {
+			if (!tourWaypoints_latLngReversed?.length) return;
+			setWalkingWaypoints(
+				(await service_routeService.getRoute(tourWaypoints_latLngReversed))
+					.features[0].geometry.coordinates
+			);
+		})();
 	}, [state_TourState]);
 
 	return (
 		<>
 			{state_TourState && (
-				<>
-					<Pane name="tour-stops" style={{ zIndex: 250 }}>
-						<MarkerLayer>
-							{state_TourState?.stops.map((stop) => (
-								<Marker
-									key={stop.id}
-									position={{
-										lat: stop.stop_location.latitude,
-										lng: stop.stop_location.longitude,
-									}}
-									placement="bottom"
-									size={[0, 0]}
-								>
-									<TourPin size={40}>{(stop.order + 1).toString()}</TourPin>
-								</Marker>
-							))}
-						</MarkerLayer>
-					</Pane>
-					<Pane name="tour-route" style={{ zIndex: 210 }}>
-						<Polyline
-							pathOptions={{
-								color: _g.theme.palette.primary.main,
-							}}
-							positions={tourWaypoints}
-						/>
-					</Pane>
-				</>
+				<Pane name="tour-stops" style={{ zIndex: 250 }}>
+					<MarkerLayer>
+						{state_TourState?.stops.map((stop) => (
+							<Marker
+								key={stop.id}
+								position={{
+									lat: stop.stop_location.latitude,
+									lng: stop.stop_location.longitude,
+								}}
+								placement="bottom"
+								size={[0, 0]}
+							>
+								<TourPin size={40}>
+									{stop.type === 'bigStop' ? (
+										(stop.order + 1).toString()
+									) : (
+										<i className="fa-solid fa-store scale-125" />
+									)}
+								</TourPin>
+							</Marker>
+						))}
+					</MarkerLayer>
+				</Pane>
 			)}
+
+			<Pane name="tour-route" style={{ zIndex: 210 }}>
+				<Polyline
+					pathOptions={{
+						color: _g.theme.palette.primary.main,
+					}}
+					positions={walkingWaypoints.map((w) => [w[1], w[0]])}
+				/>
+			</Pane>
 		</>
 	);
 });
---
+++ apps/tour-viewer/src/shared/services/route.service.ts
@@ -0,0 +1,15 @@
+import openrouteservice from 'openrouteservice-js';
+
+const Directions = new openrouteservice.Directions({
+	api_key: '5b3ce3597851110001cf6248ad8483d00dbb489e958c465e60ca3baa',
+});
+
+export const service_routeService = {
+	getRoute: async (waypoints: [number, number][]) => {
+		return await Directions.calculate({
+			coordinates: waypoints,
+			profile: 'foot-walking',
+			format: 'geojson',
+		});
+	},
+};
