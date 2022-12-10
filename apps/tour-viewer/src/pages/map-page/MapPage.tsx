c808cd705d94109b1706e2aa3b4ee4a8b5c28e25
Assaf Koren <assafnoahkoren@gmail.com>
2022-12-10 22:45:17 +0200

    added tour stops and route to map

--- apps/tour-viewer/src/pages/map-page/MapPage.tsx
+++ apps/tour-viewer/src/pages/map-page/MapPage.tsx
@@ -6,16 +6,20 @@ import { OfflineTileLayer } from '@app/features/map/offline-tile-layer/OfflineTi
 import { TourLayer } from '@app/features/map/tour-layer/TourLayer';
 import 'leaflet/dist/leaflet.css';
 
-export const MapPage = React.memo(
-	function MapPage() {
-		const position: LatLngExpression = [31.788009038677018, 34.62986136174621];
-		const bounds: LatLngBoundsExpression = [[31.788009038677018, 34.62986136174621], [31.788009038677018, 34.62986136174621]]
-		return (
-			<Box className='h-full w-full'>
-				<MapContainer className='h-full w-full' center={position} zoom={13} zoomControl={false} bounds={bounds}>
-					<OfflineTileLayer />
-					<TourLayer />
-				</MapContainer>
-			</Box>
-		);
-	});
+export const MapPage = React.memo(function MapPage() {
+	const position: LatLngExpression = [31.788009038677018, 34.62986136174621];
+
+	return (
+		<Box className="h-full w-full">
+			<MapContainer
+				className="h-full w-full"
+				center={position}
+				zoom={13}
+				zoomControl={false}
+			>
+				<OfflineTileLayer />
+				<TourLayer />
+			</MapContainer>
+		</Box>
+	);
+});
