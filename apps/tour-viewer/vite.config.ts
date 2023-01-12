645ac7bba791bc56bc6532ea05bf42048681a097
Assaf Noah Koren <assafnoahkoren@gmail.com>
2023-01-12 08:25:31 +0200

    .

--- apps/tour-viewer/vite.config.ts
+++ apps/tour-viewer/vite.config.ts
@@ -8,6 +8,7 @@ import { vitePwaConfig } from './src/core/pwa/vite-pwa-config';
 //@ts-ignore
 import { rollupReplaceConfig } from './src/core/pwa/rollup-replace-config';
 import { VitePWA } from 'vite-plugin-pwa';
+//@ts-ignore
 
 export default defineConfig({
 	plugins: [
@@ -25,11 +26,36 @@ export default defineConfig({
 			registerType: 'autoUpdate',
 			mode: 'development',
 			base: '/',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,woff2,jpg*}'],
+				runtimeCaching: [
+					{
+						urlPattern: () => true,
+						handler: 'StaleWhileRevalidate',
+						options: {
+							cacheName: 'api-cache',
+							cacheableResponse: {
+								statuses: [200]
+							}
+						},
+					},
+					{
+						urlPattern: () => true,
+						handler: 'NetworkFirst',
+						method: 'POST',
+						options: {
+							cacheName: 'api-cache-post',
+							cacheableResponse: {
+								statuses: [200]
+							}
+						},
+					}]
+			},
 			devOptions: {
-			  enabled: true,
-			  type: 'module'
+				enabled: true,
+				type: 'module'
 			}
-		  }),
+		}),
 		rollupReplaceConfig(),
 	],
 	publicDir: './src/assets',
